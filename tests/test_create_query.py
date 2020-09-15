1c9a10bb7337e1eec1a900f8ef5f7c803f061737
oliveriopt <oliver.pozo.m@gmail.com>
2020-09-15 19:12:30 -0300

    Unit tests for create query

---
+++ tests/test_create_query.py
@@ -0,0 +1,13 @@
+import db_interface.src.cons_database as cons
+
+from unittest import TestCase
+from db_interface.src.create_query import BuildInjectQuery
+
+
+class TestBuildInjectQuery(TestCase):
+
+    def test_build_query_insert(self):
+        pass
+
+    def test_function_insert(self):
+        pass
