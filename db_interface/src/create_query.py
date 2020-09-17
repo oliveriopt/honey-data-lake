298ca33e9eda6febd19946c238c661c17207d3a4
oliveriopt <oliver.pozo.m@gmail.com>
2020-09-17 10:23:35 -0300

    Improve create_query.py

--- db_interface/src/create_query.py
+++ db_interface/src/create_query.py
@@ -9,7 +9,7 @@ logger = logging.getLogger()
 class BuildInjectQuery:
 
     @staticmethod
-    def build_query_insert(table, key_duplicate, list_values):
+    def build_query_insert(table: str, key_duplicate: bool, list_values: list):
         '''
         Build query Insert
         :param key_duplicate : boolean in oredr to have dupliacte key on query
@@ -24,13 +24,14 @@ class BuildInjectQuery:
 
         q = BuildInjectQuery.function_insert(PostgreSQLQuery.into(table).insert(values_table[0]), values_table, table,
                                              0)
+        print(type(q))
         if key_duplicate:
             q = q.on_conflict(table.id).do_nothing()
 
         return str(q)
 
     @staticmethod
-    def function_insert(q, values, table, i):
+    def function_insert(q: PostgreSQLQuery, values: list, table: str, i: int):
         '''
         Isnert values of list on query
         :param q : list of entities
